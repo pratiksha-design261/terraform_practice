989fd0a440e38bb2e013d836cea19cb2978ee293
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-08-19 16:05:27 +0530

    Create terraform.tf
    this file will enable aws and azur env to use

---
+++ sample_code/terraform.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.63.0"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.116.0"
+    }
+  }
+}
